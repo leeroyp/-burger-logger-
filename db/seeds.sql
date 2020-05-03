a2243d9734efa602f19f24ef19428eed1a9e38c0
Leeroy Phili <leeroyphili@gmail.com>
2020-05-02 23:47:37 -0400

    added a db file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ("The Beast Burger");
+INSERT INTO burgers (burger_name) VALUES ("If you dare Cheeseburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Try me Burger", true);
--- server.js
+++ server.js
@@ -1,45 +1,27 @@
-// Dependencies
 var express = require("express");
-var exphbs = require("express-handlebars");
 
-// Create an instance of the express app.
+var PORT = process.env.PORT || 3000;
+
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// Set Handlebars as the default templating engine.
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Data
-var lunches = [
-  {
-    lunch: "Beet & Goat Cheese Salad with minestrone soup."
-  }, {
-    lunch: "Pizza, two double veggie burgers, fries with a Big Gulp"
-  }
-];
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
 
-// Routes
-app.get("/weekday", function(req, res) {
-  res.render("index", lunches[0]);
-});
+app.use(routes);
 
-app.get("/weekend", function(req, res) {
-  res.render("index", lunches[1]);
-});
-
-app.get("/lunches", function(req, res) {
-  res.render("all-lunches", {
-    foods: lunches,
-    eater: "david"
-  });
-});
-
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("App now listening at localhost:" + PORT);
 });
